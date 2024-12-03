c073c81ad480b588ed23fbc3454eb905c7b222b4
Sspayne321 <samuelpayne2020@gmail.com>
2024-12-03 15:46:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function showSection(sectionNumber) {
   const tabs = document.querySelectorAll('.tab');
   const contents = document.querySelectorAll('.content');
 
-  // Remove 'active' class from all tabs and contents
   tabs.forEach(tab => tab.classList.remove('active'));
   contents.forEach(content => content.classList.remove('active'));
 
-  // Add 'active' class to the clicked tab and corresponding section
   tabs[sectionNumber - 1].classList.add('active');
   document.getElementById('section' + sectionNumber).classList.add('active');
 }
